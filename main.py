ca7ac3d4548eb11da8eae529c198a8905b17891a
bbejczy <bbejczy@icloud.com>
2020-09-29 16:41:12 +0200

    Name change for main

---
+++ main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 26 18:00:07 2020
+
+@author: bbejc
+"""
+
+import numpy as np
+import pandas as pd
+
+from matplotlib.pyplot import figure, plot, title, legend, xlabel, ylabel, show
+
+filename = '../02450_E20_Project/data/wine.data' #add the first folder in this line to the python workspace 
+df = pd.read_csv(filename, header=None)
+
+raw_data = df.to_numpy()
+
+
+cols = range(1, 14)
+X = raw_data[:, cols]
+Xclass = raw_data[:,0]
+
+
+N, M = X.shape
