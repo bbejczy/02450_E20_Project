4db40ac735420450a30c4136f72572ae73257b86
CM134 <cm@christian-metz.de>
2020-11-10 15:24:28 +0100

    regulation parameter

---
+++ Project_2/Regulisation_Parameter.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 10 13:15:22 2020
+
+Regulisation Parameter
+
+@author: cm
+"""
+
+
+from matplotlib.pylab import (figure, semilogx, loglog, xlabel, ylabel, legend, 
+                           title, subplot, show, grid)
+import numpy as np
+from scipy.io import loadmat
+import sklearn.linear_model as lm
+from sklearn import model_selection
+from toolbox_02450 import rlr_validate
+
+import sklearn.linear_model as lm
+
+from dataProcessing import *
+import dataProcessing as dP
+
+
+#%% Import data 
+
+
+raw_data,X,y,C,N,M,cols = importData(filename) #importing the raw data from the file
+
+X = standardizeData(X)
+
+# Add offset attribute
+X = np.concatenate((np.ones((X.shape[0],1)),X),1)
+attributeNames = [u'Offset']+attributeNames
+M = M+1
+
+
+#%% Regression Parameter
+
+cvf = 10
+lambdas = np.power(10.,range(-8,8))
+coef = np.empty((cvf,len(X[1])))
+
+CV = model_selection.KFold(cvf, shuffle=True)
+# M = X.shape[1]
+w = np.empty((M,cvf,len(lambdas)))
+train_error = np.empty((cvf,len(lambdas)))
+test_error = np.empty((cvf,len(lambdas)))
+f = 0
+y = y.squeeze()
+for train_index, test_index in CV.split(X,y):
+    X_train = X[train_index]
+    y_train = y[train_index]
+    X_test = X[test_index]
+    y_test = y[test_index]
+        
+    # Standardize the training and set set based on training set moments
+    # mu = np.mean(X_train[:, 1:], 0)
+    # sigma = np.std(X_train[:, 1:], 0)
+    
+    # X_train[:, 1:] = (X_train[:, 1:] - mu) / sigma
+    # X_test[:, 1:] = (X_test[:, 1:] - mu) / sigma
+    
+    # precompute terms
+    Xty = X_train.T @ y_train
+    XtX = X_train.T @ X_train
+    for l in range(0,len(lambdas)):
+        # Compute parameters for current value of lambda and current CV fold
+        # note: "linalg.lstsq(a,b)" is substitue for Matlab's left division operator "\"
+        lambdaI = lambdas[l] * np.eye(M)
+        lambdaI[0,0] = 0 # remove bias regularization
+        w[:,f,l] = np.linalg.solve(XtX+lambdaI,Xty).squeeze()
+               
+        # Evaluate training and test performance
+        train_error[f,l] = np.power(y_train-X_train @ w[:,f,l].T,2).mean(axis=0)
+        test_error[f,l] = np.power(y_test-X_test @ w[:,f,l].T,2).mean(axis=0)
+
+    f=f+1
+
+opt_val_err = np.min(np.mean(test_error,axis=0))
+opt_lambda = lambdas[np.argmin(np.mean(test_error,axis=0))]
+train_err_vs_lambda = np.mean(train_error,axis=0)
+test_err_vs_lambda = np.mean(test_error,axis=0)
+mean_w_vs_lambda = np.squeeze(np.mean(w,axis=1))
+
+Error_train = np.mean(train_error)
+Error_test = np.mean(test_error)
+
+# return opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda
+
+# Plot
+
+figure(1,figsize=(12,8))
+subplot(1,2,1)
+semilogx(lambdas,mean_w_vs_lambda.T[:,1:],'.-') # Don't plot the bias term
+xlabel('Regularization factor')
+ylabel('Mean Coefficient Values')
+grid()
+# You can choose to display the legend, but it's omitted for a cleaner 
+# plot, since there are many attributes
+#legend(attributeNames[1:], loc='best')
+
+subplot(1,2,2)
+title('Optimal lambda: 1e{0}'.format(np.log10(opt_lambda)))
+loglog(lambdas,train_err_vs_lambda.T,'b.-',lambdas,test_err_vs_lambda.T,'r.-')
+xlabel('Regularization factor')
+ylabel('Squared error (crossvalidation)')
+legend(['Train error','Validation error'])
+grid()
+
+#%% Normal linear Regression without Regulisation parameter
+cvf = 10
+CV = model_selection.KFold(cvf, shuffle=True)
+f = 0
+Error_train_noReg = np.empty(cvf)
+Error_test_noReg = np.empty(cvf)
+
+for train_index, test_index in CV.split(X,y):
+    X_train = X[train_index]
+    y_train = y[train_index]
+    X_test = X[test_index]
+    y_test = y[test_index]
+    
+    
+    # X_train[:, 1:] = (X_train[:, 1:] - mu) / sigma
+    # X_test[:, 1:] = (X_test[:, 1:] - mu) / sigma
+        
+    m = lm.LinearRegression().fit(X_train, y_train)
+    y_train_est = m.predict(X_train)
+    y_test_est = m.predict(X_test)
+    Error_train_noReg[f] = np.sum(np.square(y_train-y_train_est))/len(y_train)
+    Error_test_noReg[f] = np.sum(np.square(y_test-y_test_est))/len(y_test)
